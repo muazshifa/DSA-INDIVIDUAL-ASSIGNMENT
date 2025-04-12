f721407ce32d1bd1a63cb559853c91d8153cb104
muazshifa <shifamuaz218@gmail.com>
2025-04-12 21:46:54 +0300

    Add the smallest c++ code in an array

---
+++ smallest.cpp
@@ -0,0 +1,21 @@
+#include <iostream> 
+using namespace std; 
+ 
+int findSmallestNumber(int arr[], int size) { 
+    int smallest = arr[0]; 
+    for (int i = 1; i < size; i++) { 
+        if (arr[i] < smallest) { 
+            smallest = arr[i]; 
+        } 
+    } 
+    return smallest; 
+} 
+ 
+int main() { 
+    int arr[] = {10, 20, 30, 40, 50}; 
+    int size = sizeof(arr) / sizeof(arr[0]); 
+    int smallest = findSmallestNumber(arr, size); 
+    cout << "The smallest number in the array is: " << smallest << 
+endl; 
+    return 0;
+}
